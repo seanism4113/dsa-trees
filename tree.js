3d1ee882d5460f632c2ee174ec03ec95262fb1bf
Sean Flaherty <seanflaherty@mac.lan>
2024-12-09 15:43:14 -0500

    commit all files for trees exercise

---
+++ tree.js
@@ -0,0 +1,80 @@
+/** TreeNode: node for a general tree. */
+
+class TreeNode {
+	constructor(val, children = []) {
+		this.val = val;
+		this.children = children;
+	}
+}
+
+class Tree {
+	constructor(root = null) {
+		this.root = root;
+	}
+
+	/** sumValues(): add up all of the values in the tree. */
+
+	sumValues() {
+		if (!this.root) return 0;
+
+		let total = this.root.val;
+
+		const sumHelper = (node) => {
+			for (let child of node.children) {
+				total += child.val;
+
+				if (child.children.length > 0) {
+					sumHelper(child);
+				}
+			}
+		};
+
+		sumHelper(this.root);
+		return total;
+	}
+
+	/** countEvens(): count all of the nodes in the tree with even values. */
+
+	countEvens() {
+		if (!this.root) return 0;
+
+		let count = this.root.val % 2 === 0 ? 1 : 0;
+
+		const evensHelper = (node) => {
+			for (let child of node.children) {
+				if (child.val % 2 === 0) {
+					count += 1;
+				}
+				if (child.children.length > 0) {
+					evensHelper(child);
+				}
+			}
+		};
+		evensHelper(this.root);
+		return count;
+	}
+
+	/** numGreater(lowerBound): return a count of the number of nodes
+	 * whose value is greater than lowerBound. */
+
+	numGreater(lowerBound) {
+		if (!this.root) return 0;
+
+		let count = this.root.val > lowerBound ? 1 : 0;
+
+		const greaterHelper = (node) => {
+			for (let child of node.children) {
+				if (child.val > lowerBound) {
+					count += 1;
+				}
+				if (child.children.length > 0) {
+					greaterHelper(child);
+				}
+			}
+		};
+		greaterHelper(this.root);
+		return count;
+	}
+}
+
+module.exports = { Tree, TreeNode };
